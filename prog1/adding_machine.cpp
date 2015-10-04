2a6c0de2b899e67c24f1fbdaa867af112516899b
young-geng <young.gengxy@gmail.com>
2015-10-04 16:48:43 -0700

    first prog

---
+++ prog1/adding_machine.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main() {
+    int total = 0, subtotal = 0, temp;
+    bool flag = false;
+    while (!cin.eof()) {
+        cin >> temp;
+        if (temp == 0) {
+            if (flag) {
+                cout << total << endl;
+                break;
+            } else {
+                cout << subtotal << endl;
+                subtotal = 0;
+                flag = true;
+            }
+        } else {
+            subtotal += temp;
+            total += temp;
+            flag = false;
+        }
+    }
+    return 0;
+}
+
+            
+            
