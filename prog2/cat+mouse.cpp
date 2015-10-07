dfef2c81494749a4a4a4db0ed036d0a2498f6542
young-geng <young.gengxy@gmail.com>
2015-10-06 22:33:16 -0700

    addest test

--- prog2/cat+mouse.cpp
+++ prog2/cat+mouse.cpp
@@ -4,16 +4,24 @@
 
 using namespace std;
 
+
+bool verbose = true;
+
 // You define the GetPositions function.
 // It should read legal cat and mouse positions from the user
 // and return the position values in its two arguments.
 void GetPositions (Position& cat, Position& mouse) {
     float cat_r, cat_a, mouse_a;
-    cout << "Cat radius: ";
+    if (verbose)
+        cout << "Cat radius: ";
     cin >> cat_r;
-    cout << "Cat angle: ";
+
+    if (verbose)
+        cout << "Cat angle: ";
     cin >> cat_a;
-    cout << "Mouse angle: ";
+
+    if (verbose)
+        cout << "Mouse angle: ";
     cin >> mouse_a;
     cat.SetAbsolutePosition(cat_r, cat_a * M_PI / 180);
     mouse.SetAbsolutePosition(1.0, mouse_a * M_PI / 180);
@@ -26,11 +34,13 @@ void GetPositions (Position& cat, Position& mouse) {
 // catch the mouse, or 30 time units will go by and the cat will 
 // give up.
 void RunChase (Position &cat, Position &mouse) {
-    cout << endl << "Cat:  ";
-    cat.Print();
-    cout << "Mouse:  ";
-    mouse.Print();
-    cout << endl;
+    if (verbose) {
+        cout << endl << "Cat:  ";
+        cat.Print();
+        cout << "Mouse:  ";
+        mouse.Print();
+        cout << endl;
+    }
     for (int i = 1; i <= 30; i++) {
         Position original_cat = cat;
         if (cat.Sees(mouse)) {
@@ -39,22 +49,27 @@ void RunChase (Position &cat, Position &mouse) {
             cat.IncrementPosition(0, 1.25);
         }
         mouse.IncrementPosition(0, 1);
-        cout << "Time:  " << i << endl;
-        cout << "Cat:  ";
-        cat.Print();
-        cout << "Mouse:  ";
-        mouse.Print();
-        cout << endl;
+        if (verbose) {
+            cout << "Time:  " << i << endl;
+            cout << "Cat:  ";
+            cat.Print();
+            cout << "Mouse:  ";
+            mouse.Print();
+            cout << endl;
+        }
 
         if (cat.IsAtStatue() && mouse.IsBetween(original_cat, cat)) {
-            cout << "The cat caught the mouse!" << endl;
+            cout << "The cat caught the mouse at step " << i << endl;
             return;
         }
     }
-    cout << "The mouse escaped!" << endl;
+    cout << "The mouse escaped" << endl;
 }
 
-int main () {
+int main (int argc, char *argv[]) {
+    if (argc == 2 && string("-q").compare(argv[1]) == 0) {
+        verbose = false;
+    }
     Position cat, mouse;
     GetPositions (cat, mouse);
     RunChase (cat, mouse);
